36ed3e58a79f66615a0c00acf0c1e33c1632544c
Alexander Zhukov <zhukovaa90@gmail.com>
2017-12-16 09:55:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import codecs
+
+with codecs.open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='doh',
+    description='Asynchronous DNS-over-HTTPS client for Python',
+    long_description=readme,
+    keywords='python dns https asyncio',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    py_modules=['doh'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
